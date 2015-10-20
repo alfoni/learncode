f90c178aedf1d59612c3795c6ee0162bfd44149d
Christian Alfoni <christianalfoni@gmail.com>
2015-10-20 20:14:24 +0200

    Added courses to lazy loading

--- app/modules/courses/Courses.js
+++ app/modules/courses/Courses.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import {Decorator as Cerebral} from 'cerebral-react';
-import Toolbar from './components/Toolbar.js';
-import styles from './Courses.css';
+
+let Toolbar = null;
+let styles = null;
 
 @Cerebral({
   isLoading: ['courses', 'isLoading']
 })
 class Courses extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      canRender: false
+    };
+  }
+  componentDidMount() {
+    require.ensure([], (require) => {
+      Toolbar = require('./components/Toolbar.js');
+      styles = require('./Courses.css');
+      this.setState({
+        canRender: true
+      });
+    });
+  }
   render() {
-    return (
-      <div className={styles.wrapper} onClick={() => this.props.signals.courses.appClicked()}>
-        <Toolbar/>
-        <h1>Courses</h1>
-      </div>
-    );
+    if (this.state.canRender) {
+      return (
+        <div className={styles.wrapper} onClick={() => this.props.signals.courses.appClicked()}>
+          <Toolbar/>
+          <h1>Courses</h1>
+        </div>
+      );
+    }
+    
+    return null;
   }
 }
 
