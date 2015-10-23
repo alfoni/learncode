a9ae6e94cd708c184a1e0a7dbbf78b4d29c3330c
Tommy Østgaard <tommy.ostgaard@gmail.com>
2015-10-23 19:09:37 +0200

    Duration slider

--- app/modules/course/components/DurationSlider.js
+++ app/modules/course/components/DurationSlider.js
@@ -1,14 +1,69 @@
 import React from 'react';
+import {Decorator as Cerebral} from 'cerebral-react';
 import styles from './DurationSlider.css';
 
-function DurationSlider() {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.line}>
-        <div className={styles.dot}></div>
+@Cerebral({
+  recorder: ['course', 'recorder']
+}, {
+  currentScene: ['currentScene']
+})
+class DurationSlider extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      timer: 0,
+      interval: null
+    };
+  }
+  componentWillMount() {
+    const duration = this.props.currentScene.recording ? this.props.currentScene.recording.duration : 0;
+    this.setState({ // why?
+      duration: duration
+    });
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.recorder === prevProps.recorder) {
+      return;
+    }
+
+    if (this.props.recorder.isPlaying) {
+      this.startInterval();
+    } else {
+      this.stopInterval();
+    }
+  }
+  startInterval() {
+    this.setState({
+      interval: setInterval(() => {
+        if (this.props.recorder.isPlaying) {
+          this.setState({timer: this.state.timer + 1000});
+        }
+      }, 1000)
+    });
+  }
+  stopInterval() {
+    clearInterval(this.state.interval);
+    this.setState({
+      interval: null
+    });
+  }
+  handlerPosition() {
+    let handlerPosition = 0;
+    const duration = (this.state.duration / 1000).toFixed() - 1;
+    const timer = (this.state.timer / 1000).toFixed();
+    handlerPosition = timer / duration * 100 + '%';
+
+    return handlerPosition;
+  }
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.line}>
+          <div className={styles.dot} style={{left: this.handlerPosition()}}></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default DurationSlider;
