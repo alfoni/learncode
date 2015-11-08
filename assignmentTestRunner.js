63dca6f85650b08151d660bfe251ddee72a175a7
Christian Alfoni <christianalfoni@gmail.com>
2015-11-08 19:54:10 +0100

    Fixed handling of assignment running

--- assignmentTestRunner.js
+++ assignmentTestRunner.js
@@ -8,6 +8,6 @@
     window.parent.postMessage({
       signal: 'sandboxTested',
       message: result
-    }, process.env.NODE_ENV === 'production' ? 'http://www.kodeboksen.no' : 'http://learncode.com:3000');
+    }, location.origin);
   });
 }(window));
