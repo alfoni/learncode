7c9a02cd884314acae938952f0c37ec037d86191
Tommy Østgaard <tommy.ostgaard@gmail.com>
2015-11-11 12:53:45 +0100

    Fixed mouse cursor check

--- app/modules/course/components/MouseCursor.js
+++ app/modules/course/components/MouseCursor.js
@@ -16,8 +16,7 @@ class MouseCursor extends React.Component {
   }
   render() {
     return (
-      // this.props.isPlaying && 
-        !this.hasSwitched ?
+      this.props.isPlaying && !this.hasSwitched ?
           <div
             key={this.props.mousePosition.x + '_' + this.props.mousePosition.y}
             className={styles.wrapper}
