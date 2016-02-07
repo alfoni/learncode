39a1ae73f7ef82f2dbbf56439a848fe5f24fa8e7
Christian Alfoni <christianalfoni@gmail.com>
2016-02-07 14:07:33 +0100

    Fixed saving over solved assignment and removed offset on mouse Y

--- app/modules/course/signals/runAssignmentClicked.js
+++ app/modules/course/signals/runAssignmentClicked.js
@@ -1,9 +1,15 @@
 import set from 'cerebral-addons/set';
 import setLoadingAssignment from '../actions/setLoadingAssignment.js';
 import saveSandboxChain from '../chains/saveSandbox';
+import assignmentSolved from '../actions/assignmentSolved';
 
 export default [
-  set('state://./currentAssignmentStatus.result', false),
-  setLoadingAssignment,
-  ...saveSandboxChain
+  assignmentSolved, {
+    true: [...saveSandboxChain],
+    false: [
+      set('state://./currentAssignmentStatus.result', false),
+      setLoadingAssignment,
+      ...saveSandboxChain
+    ]
+  }
 ];
