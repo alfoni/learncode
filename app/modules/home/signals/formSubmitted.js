f0e6f038656afd38ac98c7ee87164ab62cb26e93
Christian Alfoni <christianalfoni@gmail.com>
2016-02-11 00:54:58 +0100

    Set start course to introduction

--- app/modules/home/signals/formSubmitted.js
+++ app/modules/home/signals/formSubmitted.js
@@ -12,7 +12,7 @@ export default [
         setUser,
         redirect(
           process.env.NODE_ENV === 'production' ?
-            '/courses/ff81d96c-f73d-4049-a4a3-f27b38811182/scenes/0'
+            '/courses/f644e54e-d6d1-44ae-ac97-ac5cea6be209/scenes/0'
           :
             '/courses'
           )
--- app/modules/home/signals/startCourseClicked.js
+++ app/modules/home/signals/startCourseClicked.js
@@ -3,7 +3,7 @@ import redirect from 'common/factories/actions/redirect';
 export default [
   redirect(
     process.env.NODE_ENV === 'production' ?
-      '/courses/ff81d96c-f73d-4049-a4a3-f27b38811182/scenes/0'
+      '/courses/f644e54e-d6d1-44ae-ac97-ac5cea6be209/scenes/0'
     :
       '/courses'
     )
